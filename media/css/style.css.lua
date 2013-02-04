b8d29f736386ea9c3d01c9c6ffba2567c1902862
Fabian Hirschmann <fabian@hirschm.net>
2013-02-04 17:25:49 +0100

    updated style

--- media/css/style.css.lua
+++ media/css/style.css.lua
@@ -1,8 +1,25 @@
--- vim: set ft=html:
+-- vim: set ft=css:
 css = [[
 body {
 	font-family: "Trebuchet MS", "Helvetica", "Arial",  "Verdana", "sans-serif";
 	font-size: 62.5%;
+}
+
+table {
+    border-spacing: 5px;
+}
+
+thead th {
+    text-align: left;
+}
+
+tr.even td {
+    background-color: yellow;
+    background-color: orange;
+}
+
+table tbody {
+    padding: 5px;
 }
 ]]
 return css
