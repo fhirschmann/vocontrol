1300b6cf8aec942ce3f51f990b35d3047a915ca5
Fabian Hirschmann <fabian@hirschm.net>
2013-02-05 02:05:46 +0100

    sector: fixed targeting

--- media/js/vomote.js.lua
+++ media/js/vomote.js.lua
@@ -65,7 +65,7 @@ function update() {
                       $.format(
                       "<tr id=\"targ_{0}\" class=\"nation{4}\"><td>{1} [{3}%]</td><td>{2}m</tr>",
                       p));
-                    $("#targ_" + p[0]).on("click", function() { vo('target', p[0]) });
+                    $("#targ_" + p[0]).on("click", function() { vo('target', [p[0] ]) });
                 });
             } else if (key == "chat") {
                 $.each(data[key], function(_, m) {
